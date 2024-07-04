03d9b96923049fea82041a64d51992ba39f841f9
HARA Shuhei <s1310252@sshsv212.u-aizu.ac.jp>
2024-07-04 16:07:41 +0900

    HoT: initial revision

---
+++ coin.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int heads = 0;
+    int tails = 0; 
+
+    srand(time(0));
+
+    printf("Tossing a coin...\n");
+
+    for (int i = 1; i < 4; i++) {
+        if (rand() % 2 == 0) {
+            printf("Round %d: Heads\n",i);
+            heads++;
+        } else {
+            printf("Round %d: Tails\n",i);
+            tails++;
+        }
+    }
+
+    printf("Heads: %d, Tails: %d\n", heads, tails);
+
+    return 0;
+}
